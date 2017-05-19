6bf592059bcdb4932ff20fe83d8c8bbc1d766397
Esraa Alaarag <m_israa1981@hotmail.com>
2017-05-19 15:25:43 -0400

    about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
-
+import '../App.css';
 class About extends Component {
     constructor(props) {
         super(props);
     }
     about(){
         return (
-            <div>
-                
+            <div className="About">
+            <ul>
+            <li><h1>Naomi</h1></li>
+            <li><h2>Tech Impact</h2></li>
+            <li><h1>Francheska</h1></li>
+            <li><h2>perscholas</h2></li>
+            <li><h1>Esraa</h1></li>
+            <li><h2>perscholas</h2></li>
+            </ul>
             </div>
         )
     }

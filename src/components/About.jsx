97dc10bf6a394cb343965146c00439e48440e787
naeohmi <naeohmi@gmail.com>
2017-05-19 14:44:28 -0400

    structure

---
+++ src/components/About.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class About extends Component {
+    constructor(props) {
+        super(props);
+    }
+    about(){
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+    render() {
+        return (
+            this.about()
+        )
+    }
+}
+export default About;
---
+++ src/components/Portfolio.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class Portfolio extends Component {
+    constructor(props) {
+        super(props);
+    }
+    portfolio(){
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+    render() {
+        return (
+            this.portfolio()
+        )
+    }
+}
+export default Portfolio;

3153504dd8d11b5010fdaabd38b29c649118839e
Esraa Alaarag <m_israa1981@hotmail.com>
2017-05-19 15:42:07 -0400

    link

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ import Four_o_Four from './components/Four_o_Four.jsx';
 let Navi = () => {
   return (<ul>
           <li><Link to="/">Home</Link></li>
-          <li><NavLink to="/about">About</NavLink></li>
+          <li><NavLink to="/About">About</NavLink></li>
           <li><NavLink to="/portfolio">Portfolio</NavLink></li>
           <li><a href="http://www.google.com" target="_blank">Google (External Link)</a></li>
         </ul>)
@@ -29,7 +29,7 @@ class App extends Component {
       <Navi/>
         <Switch>
           <Route path="/" exact component={Home}></Route>
-          <Route path="/aboutus" component={About}></Route>
+          <Route path="/about" component={About}></Route>
           <Route path="/portfolio" component={Portfolio}></Route>
           <Route path="/*" component={Four_o_Four}></Route>
         </Switch>

68826c0ca8901263d1d1728012a3f3b4d9ebc36e
naeohmi <naeohmi@gmail.com>
2017-05-19 15:36:43 -0400

    fixed tags

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -10,13 +10,13 @@ class Projects extends Component {
                 <h1>Look at our incredible Projects! :D</h1>
                 <ul>
                     <li><a href="#">Project 1</a></li>
-                        <a href="/portfolio/:id"><img src="http://placehold.it/350x150"></a>
+                        <a href="/portfolio/:id"><img src="http://placehold.it/350x150" /></a>
 
                     <li><a href="#">Project 2</a></li>
-                        <a href="/portfolio/:id"><img src="http://placehold.it/350x150"></a>
+                        <a href="/portfolio/:id"><img src="http://placehold.it/350x150" /></a>
 
                     <li><a href="#">Project 3</a></li>
-                        <a href="/portfolio/:id"><img src="http://placehold.it/350x150"></a>
+                        <a href="/portfolio/:id"><img src="http://placehold.it/350x150" /></a>
                 </ul>
             </div>
         )
